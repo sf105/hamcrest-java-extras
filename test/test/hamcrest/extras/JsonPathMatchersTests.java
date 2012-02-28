7d6d858fab8dfc604fc5438f453643df709aa81c
Steve Freeman <github@m3p.co.uk>
2012-02-28 21:15:11 +0100

    handles bad json and missing top level path

--- test/test/hamcrest/extras/JsonPathMatchersTests.java
+++ test/test/hamcrest/extras/JsonPathMatchersTests.java
@@ -2,12 +2,15 @@ package test.hamcrest.extras;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeDiagnosingMatcher;
 import org.junit.Test;
 
 import static test.hamcrest.extras.AbstractMatcherTestSupport.assertMatches;
+import static test.hamcrest.extras.AbstractMatcherTestSupport.assertMismatchDescription;
+import static test.hamcrest.extras.JsonPathMatchersTests.JsonPathMatcher.hasJsonPath;
 
 public class JsonPathMatchersTests {
     public final static String JSON_SRC =
@@ -17,20 +20,46 @@ public class JsonPathMatchersTests {
     @Test public void
     matches_one_level_path() {
         assertMatches("top level field", hasJsonPath("toplevel"), JSON_SRC);
+        assertMismatchDescription("no path matching 'nottoplevel'", hasJsonPath("nottoplevel"), JSON_SRC);
     }
 
-    private Matcher<String> hasJsonPath(final String jsonPath) {
-        return new TypeSafeDiagnosingMatcher<String>() {
-            @Override
-            protected boolean matchesSafely(String source, Description mismatchDescription) {
+    @Test public void
+    rejects_invalid_json() {
+        assertMismatchDescription(
+                "com.google.gson.stream.MalformedJsonException: Expected EOF at line 1 column 5",
+                hasJsonPath("something"),
+                "not really json");
+    }
+
+    public static class JsonPathMatcher extends TypeSafeDiagnosingMatcher<String> {
+        private final String jsonPath;
+
+        public JsonPathMatcher(String jsonPath) {
+            this.jsonPath = jsonPath;
+        }
+
+        @Override
+        protected boolean matchesSafely(String source, Description mismatch) {
+            try {
                 JsonElement json = new JsonParser().parse(source);
-                return json.getAsJsonObject().has(jsonPath);
+                boolean pathFound = json.getAsJsonObject().has(jsonPath);
+                if (!pathFound) {
+                    mismatch.appendText("no path matching '").appendText(jsonPath).appendText("'");
+                    return false;
+                }
+                return true;
+            } catch (JsonSyntaxException e) {
+                mismatch.appendText(e.getMessage());
             }
+            return false;
+        }
 
-            public void describeTo(Description description) {
-               description.appendText("Json with path '").appendText(jsonPath).appendText("'");
-            }
-        };
+        public void describeTo(Description description) {
+           description.appendText("Json with path '").appendText(jsonPath).appendText("'");
+        }
+
+        public static Matcher<String> hasJsonPath(final String jsonPath) {
+            return new JsonPathMatcher(jsonPath);
+        }
     }
-
 }
